f8d3f1630d65816e1cd89f5941bcef437a5ed5d4
Xiaohan <969587967@qq.com>
2023-12-24 18:49:51 +0800

    完成了游戏地图内公共场景的搭建，可以接入每个地图的路径坐标进行初始化

---
+++ Classes/GameScene.h
@@ -0,0 +1,63 @@
+#ifndef __GAME_SCENE_H__
+#define __GAME_SCENE_H__
+
+#include "cocos2d.h"
+#include <vector>
+using namespace cocos2d;
+using namespace std;
+
+class GameScene : public Scene
+{
+private:
+    LayerColor* maskLayer;      // 蒙版,添加在类内是为了屏蔽暂停时对地图的操作
+    bool isGamePaused = false;  // 跟踪暂停状态
+public:
+    static Scene* createScene();
+
+    virtual bool init();
+
+    void options();
+
+    void createCountdownAnimation();
+
+    CREATE_FUNC(GameScene);
+};
+
+class GameMap : public Layer
+{
+public:
+    // 初始化方法
+    virtual bool init();
+
+    // 定义网格大小 12X8
+    static const int GRID_WIDTH = 12;
+    static const int GRID_HEIGHT = 8;
+
+    // 格子结构
+    struct Grid {
+        int x, y;
+    };
+
+    // 创建格子
+    vector<vector<bool>> gridMap;
+
+    // 存储路径
+    vector<Grid> path;
+
+    // 转换函数，将网格坐标转换为像素坐标
+    Vec2 gridToPixel(int x, int y);
+
+    // 逆转换函数，将像素坐标转换为网格坐标
+    Grid pixelToGrid(float x, float y);
+
+    // 方法来初始化网格和路径
+    void setupGrid();
+    void addPathPoint(int x, int y);
+
+    // 打印放置炮台的位置
+    void printStartSprite();
+
+    CREATE_FUNC(GameMap);
+};
+
+#endif
